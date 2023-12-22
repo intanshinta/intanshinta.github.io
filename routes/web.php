9c7b22148fb7cdbfa32519780f93ea74b9239b8f
intanshinta <intanshinta13@gmail.com>
2023-12-22 11:22:06 +0700

    update route and rename

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('dashboard');
+    return view('index');
 });
 Route::get('/about', function () {
     return view('about');
